be9f6b8105e81157d30df03bd3333862cd2d6cae
feci <ferenchegymegi3@gmail.com>
2023-09-21 22:36:18 +0200

    script-training

---
+++ script.js
@@ -0,0 +1,32 @@
+/* let a = 5;
+let b = 100;
+let c = 1000;
+
+function multiply(number) {
+	console.log(number * 2);
+}
+
+multiply(a);
+multiply(b);
+multiply(c);
+ */
+
+/* let blueCar = {
+	color: "blue", price: 450000, fuel: "petrol";
+}
+let redCarPetrol = {
+	color: "red", price: 440000, fuel: "petrol";
+}
+let redCarDisel = {
+	color: "red", price: 450000, fuel: "disel";
+}
+
+function logger(text) {
+	console.log(text)
+  }
+
+  logger() */
+/* 
+  let array = ["macska", "kutya", "gyík", "cápa"]
+
+  console.log(array) */
